a49d6e7958f2c49c1b13e067407bb6a7d7a8b5e7
ilayc11 <ilayc11@gmail.com>
2024-01-29 22:35:21 +0200

    Adding ass2

---
+++ ASS/ASS_2/CodeSample.java
@@ -0,0 +1,16 @@
+public class CodeSample {
+
+    public void run(int x, int y) {
+        System.out.println("A simple statement code");
+        if (x == 3) {
+            System.out.println("A simple statement code");
+            System.out.println("A simple statement code");
+            System.out.println("A simple statement code");
+        } else {
+            System.out.println("Some more code");
+        }
+        while ((y == 0 && x < 4)) {
+            System.out.println("A simple statement code");
+        }
+    }
+}
